91839ac2397e152afc52c00aa07518bc2b69c39c
Depuydt Marion <marion.depuydt@etu.utc.fr>
2016-03-16 23:33:22 +0100

    Initial Commit

---
+++ MOOC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MOOC
+//
+//  Created by Marion Depuydt on 16/03/2016.
+//  Copyright (c) 2016 Marion Depuydt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
